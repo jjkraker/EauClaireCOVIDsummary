63ce121143122109e89a20ae0ffa7298356cba8c
Kraker <KRAKERJJ@uwec.edu>
2020-11-30 14:21:18 -0600

    Nov 30 update

--- ModelingHospAndDths.R
+++ ModelingHospAndDths.R
@@ -1,3 +1,76 @@
+dataused = WIdata
+n = dim(dataused)[1]; 
+dataused <- mutate(dataused,POS_80plus = rowSums(cbind(POS_80_89,  POS_90),na.rm=T))
+dataused <- mutate(dataused,IP_Y_80plus = rowSums(cbind(IP_Y_80_89,  IP_Y_90),na.rm=T))
+dataused <- mutate(dataused,DTHS_80plus = rowSums(cbind(DTHS_80_89,  DTHS_90),na.rm=T))
+
+dataused <- mutate(dataused,POS_50_59_7DAY = rollmean(c(0,POS_50_59[-1] - POS_50_59[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,HOSP_50_59_7DAY = rollmean(c(0,IP_Y_50_59[-1] - IP_Y_50_59[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,DTHS_50_59_7DAY = rollmean(c(0,DTHS_50_59[-1] - DTHS_50_59[-n]), 7, na.pad=TRUE))
+
+dataused <- mutate(dataused,POS_60_69_7DAY = rollmean(c(0,POS_60_69[-1] - POS_60_69[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,HOSP_60_69_7DAY = rollmean(c(0,IP_Y_60_69[-1] - IP_Y_60_69[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,DTHS_60_69_7DAY = rollmean(c(0,DTHS_60_69[-1] - DTHS_60_69[-n]), 7, na.pad=TRUE))
+
+dataused <- mutate(dataused,POS_70_79_7DAY = rollmean(c(0,POS_70_79[-1] - POS_70_79[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,HOSP_70_79_7DAY = rollmean(c(0,IP_Y_70_79[-1] - IP_Y_70_79[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,DTHS_70_79_7DAY = rollmean(c(0,DTHS_70_79[-1] - DTHS_70_79[-n]), 7, na.pad=TRUE))
+
+dataused <- mutate(dataused,POS_80plus_7DAY = rollmean(c(0,POS_80plus[-1] - POS_80plus[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,HOSP_80plus_7DAY = rollmean(c(0,IP_Y_80plus[-1] - IP_Y_80plus[-n]), 7, na.pad=TRUE))
+dataused <- mutate(dataused,DTHS_80plus_7DAY = rollmean(c(0,DTHS_80plus[-1] - DTHS_80plus[-n]), 7, na.pad=TRUE))
+
+allhosprates <- rep(0,9)
+alldthrates <- rep(0,9)
+#### plots - 80plus ####
+plot(dataused$POS_80plus_7DAY,type="l")
+hosplag = 4; dthlag=4
+hosprate = 4.3; dthrate=8.5
+points(hosplag:(n+hosplag-1),dataused$HOSP_80plus_7DAY*hosprate,type="l",col="blue")
+#plot(dataused$POS_80plus_7DAY[1:(n-hosplag+1)],dataused$HOSP_80plus_7DAY[hosplag:n])
+points(dthlag:(n+dthlag-1),dataused$DTHS_80plus_7DAY*dthrate,type="l",col="red")
+#plot(dataused$POS_80plus_7DAY[1:(n-hosplag+1)],dataused$DTHS_80plus_7DAY[hosplag:n])
+#points(dataused$POS_80plus_7DAY[186:(n-hosplag+1)],dataused$DTHS_80plus_7DAY[(hosplag+185):n],col="blue",pch=20)
+allhosprates[9] = 1/hosprate
+alldthrates[9] = 1/dthrate
+
+#### plots - 70-79 ####
+plot(dataused$POS_70_79_7DAY,type="l")
+hosplag = 7; dthlag=14
+hosprate = 6.5; dthrate=26
+points(dataused$HOSP_70_79_7DAY[hosplag:n]*hosprate,type="l",col="blue")
+points(dataused$DTHS_70_79_7DAY[dthlag:n]*dthrate,type="l",col="red")
+allhosprates[8] = 1/hosprate
+alldthrates[8] = 1/dthrate
+plot(dataused$POS_70_79_7DAY[1:(n-hosplag+1)],dataused$HOSP_70_79_7DAY[hosplag:n])
+abline(0,1/hosprate)
+plot(dataused$POS_70_79_7DAY[1:(n-dthlag+1)],dataused$DTHS_70_79_7DAY[dthlag:n])
+points(dataused$POS_70_79_7DAY[186:(n-dthlag+1)],dataused$DTHS_70_79_7DAY[(dthlag+185):n],col="blue",pch=20)
+abline(0,1/dthrate)
+
+#### plots - 60-69 ####
+plot(dataused$POS_60_69_7DAY,type="l")
+hosplag = 4; dthlag=8
+hosprate = 15; dthrate=110
+points(dataused$HOSP_60_69_7DAY[hosplag:n]*hosprate,type="l",col="blue")
+points(dataused$DTHS_60_69_7DAY[dthlag:n]*dthrate,type="l",col="red")
+allhosprates[7] = 1/hosprate
+alldthrates[7] = 1/dthrate
+
+#### plots - 50-59 ####
+plot(dataused$POS_50_59_7DAY,type="l")
+hosplag = 2; dthlag=8
+hosprate = 30; dthrate=400
+points(dataused$HOSP_50_59_7DAY[hosplag:n]*hosprate,type="l",col="blue")
+points(dataused$DTHS_50_59_7DAY[dthlag:n]*dthrate,type="l",col="red")
+allhosprates[7] = 1/hosprate
+alldthrates[7] = 1/dthrate
+
+#### end plots ###
+
+
+
+
 n = dim(ECdata)[1]; 
 
 ECdata <- mutate(ECdata,POS_50plus = rowSums(cbind(POS_50_59, POS_60_69, POS_70_79, POS_80_89,  POS_90),na.rm=T))
