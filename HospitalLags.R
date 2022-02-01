2da4f1f0eb6126e497fc2dfaf7d068a2e27fceb3
jjkraker <krakerjj@uwec.edu>
2022-02-01 16:02:30 -0600

    Jan 31 update

--- HospitalLags.R
+++ HospitalLags.R
@@ -27,3 +27,65 @@ lagdeath = 15; multdeath =250
 laghosp = 4; multhosp =12
 lagdeath = 15; multdeath =60
 
+
+
+########################
+
+# fix to go back to August 1
+patternA = (-47:45)+240
+patternB = (46:60)+240
+patternC = (61:90)+240
+patternD = (91:116)+240
+patternE = (116:280)+240
+patternF = (281:310)+240
+patternG = (311:467)+240
+  
+hosplag=10
+
+plot(WIdata$POS_7DAYAVG[241:707],WIdata$HOSP_7DAY_AVG[(241:707)+hosplag], xlim=c(0,25000))
+# A
+WIdata$DATE[patternA]
+colused=rep(NA,707)
+points(WIdata$POS_7DAYAVG[patternA],WIdata$HOSP_7DAY_AVG[(patternA)+hosplag],
+       col="red",pch=20)
+
+# B
+WIdata$DATE[patternB]
+colused=rep(NA,707)
+points(WIdata$POS_7DAYAVG[patternB],WIdata$HOSP_7DAY_AVG[(patternB)+hosplag],
+       col="orange",pch=20)
+
+colused[patternA]="red" 
+colused[patternB]="orange" 
+colused[patternC]="yellow" 
+colused[patternD]="green" 
+colused[patternE]="turquoise" 
+colused[patternF]="blue" 
+colused[patternG] = "purple"
+
+
+maxn = length(WIdata$DATE)
+newpattern = (708:(maxn-hosplag))
+WIdata$DATE[newpattern] 
+
+
+plot(WIdata$POS_7DAYAVG[241:707],WIdata$HOSP_7DAY_AVG[(241:707)+hosplag], xlim=c(0,25000))
+points(WIdata$POS_7DAYAVG[1:707],WIdata$HOSP_7DAY_AVG[(1:707)+hosplag],col=colused,pch=20)
+points(WIdata$POS_7DAYAVG[(708:(maxn-hosplag))],WIdata$HOSP_7DAY_AVG[((708+hosplag):maxn)],col="violet",pch=20)
+
+
+
+#plot(WIdata$POS_7DAYAVG[newpattern],WIdata$HOSP_7DAY_AVG[(newpattern)+hosplag])
+
+plot(WIdata$POS_7DAYAVG[patternG],WIdata$HOSP_14DAY_AVG[(patternG)+hosplag],xlim=c(0,25000),ylim=c(0,200))
+points(WIdata$POS_7DAYAVG[(611:650)],WIdata$HOSP_14DAY_AVG[(611:650)+hosplag],col="red",pch=20)
+WIdata$DATE[(611:650)]  # ?? increased testing with schooling?
+points(WIdata$POS_7DAYAVG[(681:695)],WIdata$HOSP_14DAY_AVG[(681:695)+hosplag],col="blue",pch=20)
+WIdata$DATE[(681:695)]  # post thanksgiving weirdness
+abline(60,.021)
+
+points(WIdata$POS_7DAYAVG[(708:(maxn-hosplag))],WIdata$HOSP_14DAY_AVG[((708+hosplag):maxn)],col="purple",pch=20)
+
+
+## hospitalization capacity
+## look at deaths
