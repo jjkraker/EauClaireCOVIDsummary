6fbb44076c874fd1be9d02bd7e3f67795e3b25d5
Kraker <KRAKERJJ@uwec.edu>
2022-02-20 11:03:43 -0600

    Feb. 17 update

--- HospitalLags.R
+++ HospitalLags.R
@@ -39,21 +39,11 @@ patternD = (91:116)+240
 patternE = (116:280)+240
 patternF = (281:310)+240
 patternG = (311:467)+240
+firstday <- min(patternA)
+lastday <- max(patternG)
   
-hosplag=10
+hosplag=7
 
-plot(WIdata$POS_7DAYAVG[241:707],WIdata$HOSP_7DAY_AVG[(241:707)+hosplag], xlim=c(0,25000))
-# A
-WIdata$DATE[patternA]
-colused=rep(NA,707)
-points(WIdata$POS_7DAYAVG[patternA],WIdata$HOSP_7DAY_AVG[(patternA)+hosplag],
-       col="red",pch=20)
-
-# B
-WIdata$DATE[patternB]
-colused=rep(NA,707)
-points(WIdata$POS_7DAYAVG[patternB],WIdata$HOSP_7DAY_AVG[(patternB)+hosplag],
-       col="orange",pch=20)
 
 colused[patternA]="red" 
 colused[patternB]="orange" 
@@ -69,10 +59,15 @@ newpattern = (708:(maxn-hosplag))
 WIdata$DATE[newpattern] 
 
 
-plot(WIdata$POS_7DAYAVG[241:707],WIdata$HOSP_7DAY_AVG[(241:707)+hosplag], xlim=c(0,25000))
-points(WIdata$POS_7DAYAVG[1:707],WIdata$HOSP_7DAY_AVG[(1:707)+hosplag],col=colused,pch=20)
-points(WIdata$POS_7DAYAVG[(708:(maxn-hosplag))],WIdata$HOSP_7DAY_AVG[((708+hosplag):maxn)],col="violet",pch=20)
+plot(WIdata$POS_7DAYAVG[firstday:lastday],WIdata$HOSP_7DAY_AVG[(firstday:lastday)+hosplag], xlim=c(0,25000))
+points(WIdata$POS_7DAYAVG[1:lastday],WIdata$HOSP_7DAY_AVG[(1:lastday)+hosplag],col=colused,pch=20)
+points(WIdata$POS_7DAYAVG[((lastday+1):(maxn-hosplag))],WIdata$HOSP_7DAY_AVG[((lastday+1+hosplag):maxn)],col="violet",pch=20)
 
+
+## hospitalization capacity
+## look at deaths
+
+#  weird October and November patterns
 
 
 #plot(WIdata$POS_7DAYAVG[newpattern],WIdata$HOSP_7DAY_AVG[(newpattern)+hosplag])
@@ -86,6 +81,3 @@ abline(60,.021)
 
 points(WIdata$POS_7DAYAVG[(708:(maxn-hosplag))],WIdata$HOSP_14DAY_AVG[((708+hosplag):maxn)],col="purple",pch=20)
 
-
-## hospitalization capacity
-## look at deaths
