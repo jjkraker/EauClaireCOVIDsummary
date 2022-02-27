1db468e6c3c74ab9a60b16bfefdebb2b3a0b3375
Kraker <KRAKERJJ@uwec.edu>
2022-02-27 15:46:21 -0600

    Feb 24 update

--- HospitalLags.R
+++ HospitalLags.R
@@ -1,35 +1,31 @@
-POS_daily <- WIdata2021$POS_20_29 - lag(WIdata2021$POS_20_29)
-HOSP_daily <- WIdata2021$IP_Y_20_29_CP - lag(WIdata2021$IP_Y_20_29_CP)
-DTH_daily <- WIdata2021$DTHS_20_29_CP - lag(WIdata2021$DTHS_20_29_CP)
+############### estimating changepoints ######################3
 
-POS_daily_14dayAVG = rollmean(POS_daily, 14, na.pad=TRUE)
-POS_daily_14dayAVG
-HOSP_daily_14dayAVG = rollmean(HOSP_daily, 14, na.pad=TRUE)
-HOSP_daily_14dayAVG
-DTH_daily_14dayAVG = rollmean(DTH_daily, 14, na.pad=TRUE)
-DTH_daily_14dayAVG
+# 193, 286, 301, 331, 356, 521, 551, 708
+WIdata$DATE[c(193, 286, 301, 331, 356, 521, 551, 708)] 
+PatternChangePoints = c("August 01 2020",
+                        "November 02 2020",
+                        "November 17 2020",
+                        "December 17 2020",
+                        "January 11 2021",
+                        "June 25 2021",
+                        "July 25 2021",
+                        "December 29 2021")
+PatternStages = c("P0", 
+                 "P1",
+                 "P2",
+                 "P3",
+                 "P4")
 
-laghosp = 5; multhosp =60
-lagdeath = 21; multdeath =1000
-plot(POS_daily_14dayAVG,type="l",lwd=2)
-points((1:length(HOSP_daily_14dayAVG))-laghosp,HOSP_daily_14dayAVG*multhosp,col="blue",type="l")
-points((1:length(DTH_daily_14dayAVG))-lagdeath,DTH_daily_14dayAVG*multdeath,col="red",type="l")
+whichChangeDATES = WIdata$DATE %in% PatternChangePoints
+daily_data$DATE[whichVaxDATES]
 
-#twenties
-laghosp = 5; multhosp =60
-lagdeath = 21; multdeath =1000
-
-#forties
-laghosp = 4; multhosp =35
-lagdeath = 15; multdeath =250
-
-#sixties
-laghosp = 4; multhosp =12
-lagdeath = 15; multdeath =60
-
-
-
-########################
+n_daily = dim(daily_data)[1]
+whereVaxChangePoints = (1:n_daily)[whichVaxDATES]
+daily_data$Stage = rep(NA, n_daily)
+for (i in 1:length(whereVaxChangePoints)) {
+  daily_data$Stage[(whereVaxChangePoints[i]):n_daily] = VaxStages[i]
+}
+daily_data$Stage  # NA are BEFORE we had regularly-available testing
 
 # fix to go back to August 1
 patternA = (-47:45)+240
@@ -81,3 +77,45 @@ abline(60,.021)
 
 points(WIdata$POS_7DAYAVG[(708:(maxn-hosplag))],WIdata$HOSP_14DAY_AVG[((708+hosplag):maxn)],col="purple",pch=20)
 
+
+
+
+
+
+
+
+
+############### estimating lag ######################3
+
+POS_daily <- WIdata2021$POS_20_29 - lag(WIdata2021$POS_20_29)
+HOSP_daily <- WIdata2021$IP_Y_20_29_CP - lag(WIdata2021$IP_Y_20_29_CP)
+DTH_daily <- WIdata2021$DTHS_20_29_CP - lag(WIdata2021$DTHS_20_29_CP)
+
+POS_daily_14dayAVG = rollmean(POS_daily, 14, na.pad=TRUE)
+POS_daily_14dayAVG
+HOSP_daily_14dayAVG = rollmean(HOSP_daily, 14, na.pad=TRUE)
+HOSP_daily_14dayAVG
+DTH_daily_14dayAVG = rollmean(DTH_daily, 14, na.pad=TRUE)
+DTH_daily_14dayAVG
+
+laghosp = 5; multhosp =60
+lagdeath = 21; multdeath =1000
+plot(POS_daily_14dayAVG,type="l",lwd=2)
+points((1:length(HOSP_daily_14dayAVG))-laghosp,HOSP_daily_14dayAVG*multhosp,col="blue",type="l")
+points((1:length(DTH_daily_14dayAVG))-lagdeath,DTH_daily_14dayAVG*multdeath,col="red",type="l")
+
+#twenties
+laghosp = 5; multhosp =60
+lagdeath = 21; multdeath =1000
+
+#forties
+laghosp = 4; multhosp =35
+lagdeath = 15; multdeath =250
+
+#sixties
+laghosp = 4; multhosp =12
+lagdeath = 15; multdeath =60
+
+
+
+#
